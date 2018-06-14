95052f3cf43dff3ac726cbe1d2070c996516997c
huanglu <81896631@qq.com>
2018-06-14 16:23:44 +0800

    update .gitignore

--- src/fetchConfig/env.js
+++ src/fetchConfig/env.js
@@ -10,10 +10,10 @@ let baseUrl='http://192.168.2.109:8021';
 let frontWebUrl = 'http://192.168.183.128:8091';
 if(process.env.NODE_ENV === 'production'){
 
-    frontWebUrl='http://wechatdt.xingteam.com';
-    baseUrl='http://wechatdt.xingteam.com:8101';
-    // baseUrl='http://192.168.2.114:8021';
-    // frontWebUrl = 'http://192.168.2.114:8091';
+    //frontWebUrl='http://wechatdt.xingteam.com';
+    //baseUrl='http://wechatdt.xingteam.com:8101';
+     baseUrl='http://ss.hbbdmc.com:8027';
+     frontWebUrl = 'http://ss.hbbdmc.com';
 }
 let routerMode = 'history';
 
